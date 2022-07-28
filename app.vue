4397cb2c830be256f414f117639a245c781ea203
Harlan Wilton <harlan@harlanzw.com>
2022-07-28 18:09:02 +1000

    wip: switch to mycelium

--- app.vue
+++ app.vue
@@ -3,43 +3,7 @@ const config = useTheme()
 </script>
 
 <template>
-  <Html :lang="config.site.language" dir="ltr">
-    <Head>
-      <Meta name="og:type" content="website" />
-      <Meta name="twitter:card" content="summary_large_image" />
-      <Meta property="og:locale" content="en_AU" />
-      <Meta property="og:image" :content="`${config.site.url}/cover.png`" />
-      <Meta http-equiv="Accept-CH" content="DPR" />
-      <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <Link rel="preconnect" href="https://res.cloudinary.com" />
-      <SchemaOrgPerson
-        :name="config.site.name"
-        :image="config.site.cover"
-        :same-as="config.site.sameAs"
-      />
-      <SchemaOrgWebSite
-        :name="config.site.name"
-        :image="config.site.image"
-        :description="config.site.description"
-      />
-      <SchemaOrgWebPage />
-    </Head>
-    <Body class="text-gray-800 dark:text-gray-100 antialiased">
-      <NuxtLoadingIndicator />
-      <Header />
-      <main class=" lg:(px-0) md:(max-w-700px pb-20 px-8) sm:(pb-16 px-6) mx-auto max-w-ful px-4 py-7 pb-10 l">
-        <NuxtPage class="page-enter-active" />
-      </main>
-      <footer class="px-5 sm:px-7 md:px-10 text-center text-gray-400 text-sm my-5">
-        <div class="flex justify-center mb-3">
-          <SocialIcons />
-        </div>
-        <p class="mb-3">
-          © 2022-PRESENT {{ config.site.name }}. All rights reserved.
-        </p>
-      </footer>
-    </Body>
-  </Html>
+  <Mycelium>
+    <NuxtPage />
+  </Mycelium>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,21 +5,28 @@ import config from './theme.config'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  app: {
-    /**
-     * https://nuxt-theme-kit.netlify.app
-     */
-    // TODO: Remove this ignore
-    // https://github.com/nuxt-themes/config/issues/5
-    // @ts-expect-error untyped
-    theme: {
-      meta: {
-        name: 'harlanzw.com',
-        author: '@harlan_zw',
-        description: 'My personal website built with Nuxt v3 and Nuxt Content v2. ✨',
-      },
+  extends: ['nuxt-theme-mycelium'],
+
+  components: [
+    {
+      path: './node_modules/nuxt-theme-mycelium/components/content',
+      global: true,
+      prefix: '',
+    },
+  ],
+
+  autoImports: {
+    dirs: [
+      './node_modules/nuxt-theme-mycelium/composables',
+    ],
+  },
+
+  hooks: {
+    'nuxt:composables': function () {
+
     },
   },
+
   vite: {
     css: {
       preprocessorOptions: {
@@ -30,21 +37,21 @@ export default defineNuxtConfig({
       },
     },
   },
-  modules: [
-    // Themify
-    '@nuxt-themes/config/module',
-    '@nuxtjs/design-tokens/module',
-    // Website
-    '@nuxtjs/color-mode',
-    '@vueuse/nuxt',
-    '@nuxt/image',
-    'nuxt-schema-org',
-    'nuxt-windicss',
-    // custom content modules, need to come before the content module
-    '~/modules/unplugin-icons',
-    '~/modules/content-utils',
-    '@nuxt/content',
-  ],
+  // modules: [
+  //   // Themify
+  //   '@nuxt-themes/config/module',
+  //   '@nuxtjs/design-tokens/module',
+  //   // Website
+  //   '@nuxtjs/color-mode',
+  //   '@vueuse/nuxt',
+  //   '@nuxt/image',
+  //   'nuxt-schema-org',
+  //   'nuxt-windicss',
+  //   // custom content modules, need to come before the content module
+  //  // '~/modules/unplugin-icons',
+  //  // '~/modules/content-utils',
+  //   '@nuxt/content',
+  // ],
   schemaOrg: {
     disableRuntimeScriptsWhenSSR: true,
     // To be rewritten manually by the theme user
@@ -52,71 +59,6 @@ export default defineNuxtConfig({
     // To be rewritten manually by the theme user
     defaultLanguage: config.site.language,
   },
-  css: [
-    '@/resources/scrollbars.css',
-    '@/resources/main.scss',
-  ],
-  // https://color-mode.nuxtjs.org
-  colorMode: {
-    fallback: 'dark',
-    classSuffix: '',
-  },
-  hooks: {
-    // @todo fix via nuxt, does not work
-    'components:extend': function (components) {
-      for (const component of components) {
-        if (component.chunkName.startsWith('components/prose')) {
-          component.chunkName = 'prose-components'
-          component.prefetch = false
-        }
-      }
-    },
-    'app:templates': function (app) {
-      app.templates = app.templates.map((t) => {
-        if (t.filename !== 'views/document.template.mjs')
-          return t
-
-        const analyticsScript = '<script src="https://idea-lets-dance.harlanzw.com/script.js" data-spa="auto" data-site="VDJUVDNA" defer></script>'
-
-        t.getContents = () => {
-          return `export default (params) => \`
-<!DOCTYPE html>
-<!--
-  Hey :) Thanks for inspecting my site.
-  Are you interested in the source code? You can find it here: https://github.com/harlan-zw/harlanzw.com
--->
-<html \${params.HTML_ATTRS}>
-
-<head \${params.HEAD_ATTRS}>
-  \${params.HEAD}
-</head>
-
-<body \${params.BODY_ATTRS}>\${params.BODY_PREPEND}
-  \${params.APP}
-</body>
-<!-- Start Analytics -->
-${process.env.NODE_ENV === 'production' ? analyticsScript : '<!-- Ommited -->'}
-<!-- End Analytics -->
-</html>\`
-`
-        }
-        return t
-      })
-    },
-  },
-
-  // https://content.nuxtjs.org
-  content: {
-    highlight: {
-      // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
-      theme: 'dracula',
-    },
-  },
-
-  router: {
-    trailingSlash: false,
-  },
-
   image: {
     cloudinary: {
       baseURL: 'https://res.cloudinary.com/dl6o1xpyq/image/upload/images',
