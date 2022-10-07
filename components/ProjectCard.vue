bc0fdfdcb6c1928ca771cd097fdc30ab6854e242
Harlan Wilton <harlan@harlanzw.com>
2022-10-07 19:09:38 +1100

    fix: nuxt-hedge bug

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -11,8 +11,8 @@ const props = defineProps({
   <a :href="project.link" target="_blank" class="!flex items-center space-x-3 p-4 group rounded border-gray-200/50 border-2 hover:bg-white dark:(border-gray-800/30) dark:hover:bg-gray-800 transition-all">
     <div v-if="project.icon" class="opacity-30 mr-2 group-hover:opacity-75 transition-all svg-container" v-html="project.icon" />
     <div>
-      <h2 class="mb-2 text-lg dark:text-gray-500">{{ project.name }}</h2>
-      <p class="text-xs opacity-75 dark:text-gray-600">{{ project.description }}</p>
+      <h2 class="mb-2 text-lg">{{ project.name }}</h2>
+      <p class="text-xs opacity-75">{{ project.description }}</p>
     </div>
   </a>
 </template>
