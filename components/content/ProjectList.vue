a544216cce395bc883b1f67409197cee3f3eb530
Harlan Wilton <harlan@harlanzw.com>
2023-01-05 14:36:53 +1100

    chore: bump deps

--- components/content/ProjectList.vue
+++ components/content/ProjectList.vue
@@ -12,9 +12,10 @@ const totalStars = computed(() => {
 </script>
 
 <template>
-  <div class="space-y-10 ">
-    <div class="font-bold opacity-90">
-      <i-carbon-star-filled class="inline text-yellow-400 mr-1 mb-2px" /> {{ totalStars }} stars and counting
+  <div class="space-y-15 text-center">
+    <div class="font-bold inline-flex items-center opacity-90 bg-green-400 dark:bg-green-600 text-xl px-5 py-3 rounded text-white">
+      <i-carbon-star-filled class="inline text-yellow-400 mr-2" />
+      <span>{{ totalStars }} GitHub stars <span class="opacity-70 text-xs">and counting</span></span>
     </div>
     <div v-for="(category, cKey) in categories.body" :key="cKey">
       <SubTitle>
@@ -22,7 +23,7 @@ const totalStars = computed(() => {
         <icon v-else-if="category.icon" :name="category.icon" class="w-14 h-auto mr-2 group-hover:opacity-75 transition-all svg-container" />
         {{ category.name }}
       </SubTitle>
-      <div class="grid md:grid-cols-2 gap-5">
+      <div class="grid md:grid-cols-2 gap-5 text-left">
         <ProjectCard v-for="(project, pKey) in category.projects" :key="pKey" :project="project" />
       </div>
     </div>
